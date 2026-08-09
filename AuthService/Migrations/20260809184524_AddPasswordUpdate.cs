a65b6f320bbbc3d4fe6fb66bb5107e11d642a534
mohammed x <mohammed20191700552@cis.asu.edu.eg>
2026-08-09 21:52:34 +0300

    Remove endpoint

---
+++ AuthService/Migrations/20260809184524_AddPasswordUpdate.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordChangedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 57, DateTimeKind.Utc).AddTicks(3567));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 57, DateTimeKind.Utc).AddTicks(4457));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 57, DateTimeKind.Utc).AddTicks(4458));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 58, DateTimeKind.Utc).AddTicks(6876));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 58, DateTimeKind.Utc).AddTicks(7519));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 58, DateTimeKind.Utc).AddTicks(7521));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 9, 18, 45, 24, 58, DateTimeKind.Utc).AddTicks(7521));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordChangedAt",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 751, DateTimeKind.Utc).AddTicks(3754));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 751, DateTimeKind.Utc).AddTicks(4581));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 751, DateTimeKind.Utc).AddTicks(4583));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 752, DateTimeKind.Utc).AddTicks(7448));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 752, DateTimeKind.Utc).AddTicks(8072));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 752, DateTimeKind.Utc).AddTicks(8074));
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "CreatedAt",
+                value: new DateTime(2026, 8, 7, 23, 15, 50, 752, DateTimeKind.Utc).AddTicks(8075));
+        }
+    }
+}
